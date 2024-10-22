fbb6d1c9b1b8a34fe70ae0ef0bd0e01ef8de6c9b
sweetpotato <sweetpotato@users.noreply.github.com>
2024-10-23 08:18:48 +0900

    js: no-var の修正

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@
 // [1] https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Iterator#%E3%83%96%E3%83%A9%E3%82%A6%E3%82%B6%E3%83%BC%E3%81%AE%E4%BA%92%E6%8F%9B%E6%80%A7
 //
 export function sum(array) {
-  var result = 0;
+  let result = 0;
   for (const it of array) {
     result += it;
   }
