f03b7982e8250afb25ef9ed0f0a7a440f7cd6baf
sweetpotato <sweetpotato@users.noreply.github.com>
2024-10-23 08:18:47 +0900

    js: import/prefer-default-export の抑止
    utils なので default export はそぐわない。

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
 // Iterator.prototype.reduce() を使わない総和。
 // Safari on iOS では reduce がサポートされていない[1]ため必要。
 // [1] https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Iterator#%E3%83%96%E3%83%A9%E3%82%A6%E3%82%B6%E3%83%BC%E3%81%AE%E4%BA%92%E6%8F%9B%E6%80%A7
@@ -9,3 +11,5 @@ export function sum(array) {
   }
   return result;
 }
+
+/* eslint-disable import/prefer-default-export */
