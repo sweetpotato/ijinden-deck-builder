7d5b5f8c0632713fb9319b626b216c9fc4f06e65
sweetpotato <sweetpotato@users.noreply.github.com>
2024-11-03 10:53:43 +0900

    css, js: 小サイズの ImageCard

--- src/ImageCard.jsx
+++ src/ImageCard.jsx
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
 
 function ImageCard({
-  imageUrl, alt, numCopies, hide = false, children,
+  imageUrl, alt, numCopies, small = false, hide = false, children,
 }) {
+  const width = small ? 40 : 80;
+  const height = small ? 56 : 112;
+  const containerClass = small
+    ? 'container-card card-small'
+    : 'container-card card-medium';
   return (
-    <div className="container-card" style={{ display: (hide ? 'none' : 'block') }}>
-      <img className="img-card" width="80" height="112" src={imageUrl} alt={alt} />
+    <div className={containerClass} style={{ display: (hide ? 'none' : 'block') }}>
+      <img className="img-card" width={width} height={height} src={imageUrl} alt={alt} />
       {
         numCopies !== undefined
           && <span className="container-num-copies">{numCopies}</span>
