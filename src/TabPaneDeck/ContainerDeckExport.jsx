aabe2812afe971a13fe3ee37d125793247099cab
sweetpotato <sweetpotato@users.noreply.github.com>
2025-05-05 02:26:25 +0900

    js: ContainerDeckExport をファイルに切出し

---
+++ src/TabPaneDeck/ContainerDeckExport.jsx
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+
+import { useEffect, useRef, useState } from 'react'
+import { Button, FormControl, Overlay, Tooltip } from 'react-bootstrap'
+
+import { dataCardsMap } from '../commons/dataCards'
+import { sum } from '../commons/utils'
+
+function makeTextExportedPart(title, deck) {
+  const numCards = sum(deck.values())
+  const text = [...deck.entries()]
+    .map(([id, numCopies]) => [dataCardsMap.get(id), numCopies])
+    .sort((a, b) => a[0].orderDeck - b[0].orderDeck)
+    .map(([card, numCopies]) => `\r\n${card.name}\t${numCopies}`)
+    .join('')
+  return `${title}\t${numCards}${text}`
+}
+
+function makeTextExported(deckMain, deckSide) {
+  const textMain = makeTextExportedPart('メインデッキ', deckMain)
+  const textSide = makeTextExportedPart('サイドデッキ', deckSide)
+  return `${textMain}\r\n\r\n${textSide}`
+}
+
+function ContainerDeckExport({ deckMain, deckSide }) {
+  const [showCopied, setShowCopied] = useState(false)
+  const refButton = useRef()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowCopied(false)
+    }, 1000)
+    return () => clearTimeout(timer)
+  }, [showCopied])
+
+  const textExported = makeTextExported(deckMain, deckSide)
+
+  return (
+    <>
+      <div className="m-2">
+        <Button
+          ref={refButton}
+          variant="outline-secondary"
+          onClick={async () => {
+            await navigator.clipboard.writeText(textExported)
+            setShowCopied(true)
+          }}
+        >
+          ▼テキストデータをコピー
+        </Button>
+        <Overlay target={refButton.current} show={showCopied} placement="top">
+          {(props) => <Tooltip {...props}>コピーしました</Tooltip>}
+        </Overlay>
+      </div>
+      <div className="m-2">
+        <FormControl
+          readOnly
+          as="textarea"
+          rows={deckMain.size + deckSide.size + 3}
+          value={textExported}
+        />
+      </div>
+    </>
+  )
+}
+
+export default ContainerDeckExport
--- src/TabPaneDeck/index.jsx
+++ src/TabPaneDeck/index.jsx
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-import { useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
 import {
   Alert,
   Button,
@@ -10,14 +10,9 @@ import {
   ModalFooter,
   ModalHeader,
   ModalTitle,
-  Overlay,
-  Tooltip,
 } from 'react-bootstrap'
 
-import {
-  dataCardsArrayForDeck as dataCardsArray,
-  dataCardsMap,
-} from '../commons/dataCards'
+import { dataCardsArrayForDeck as dataCardsArray } from '../commons/dataCards'
 import db from '../commons/db'
 import enumTabPane from '../commons/enumTabPane'
 import {
@@ -27,22 +22,7 @@ import {
 import { sum } from '../commons/utils'
 import ImageCard from '../components/ImageCard'
 import ContainerDeckShare from './ContainerDeckShare'
-
-function makeTextExportedPart(title, deck) {
-  const numCards = sum(deck.values())
-  const text = [...deck.entries()]
-    .map(([id, numCopies]) => [dataCardsMap.get(id), numCopies])
-    .sort((a, b) => a[0].orderDeck - b[0].orderDeck)
-    .map(([card, numCopies]) => `\r\n${card.name}\t${numCopies}`)
-    .join('')
-  return `${title}\t${numCards}${text}`
-}
-
-function makeTextExported(deckMain, deckSide) {
-  const textMain = makeTextExportedPart('メインデッキ', deckMain)
-  const textSide = makeTextExportedPart('サイドデッキ', deckSide)
-  return `${textMain}\r\n\r\n${textSide}`
-}
+import ContainerDeckExport from './ContainerDeckExport'
 
 function TabPaneDeck({
   code,
@@ -275,48 +255,6 @@ function ContainerDeckCard({
         </Button>
       </ImageCard>
     )
-  )
-}
-
-function ContainerDeckExport({ deckMain, deckSide }) {
-  const [showCopied, setShowCopied] = useState(false)
-  const refButton = useRef()
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowCopied(false)
-    }, 1000)
-    return () => clearTimeout(timer)
-  }, [showCopied])
-
-  const textExported = makeTextExported(deckMain, deckSide)
-
-  return (
-    <>
-      <div className="m-2">
-        <Button
-          ref={refButton}
-          variant="outline-secondary"
-          onClick={async () => {
-            await navigator.clipboard.writeText(textExported)
-            setShowCopied(true)
-          }}
-        >
-          ▼テキストデータをコピー
-        </Button>
-        <Overlay target={refButton.current} show={showCopied} placement="top">
-          {(props) => <Tooltip {...props}>コピーしました</Tooltip>}
-        </Overlay>
-      </div>
-      <div className="m-2">
-        <FormControl
-          readOnly
-          as="textarea"
-          rows={deckMain.size + deckSide.size + 3}
-          value={textExported}
-        />
-      </div>
-    </>
   )
 }
 
