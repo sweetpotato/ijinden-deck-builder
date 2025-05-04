55311a4022b40e3a146477625cb82d8de7d9ad70
sweetpotato <sweetpotato@users.noreply.github.com>
2025-05-04 18:22:00 +0900

    test: シミュレータの単体テストの更なる充実

--- src/TabPaneSimulator/ImageCardWithToggle.test.jsx
+++ src/TabPaneSimulator/ImageCardWithToggle.test.jsx
@@ -7,16 +7,18 @@ import userEvent from '@testing-library/user-event'
 
 afterEach(cleanup)
 
-test('レンダリング', async () => {
+test('ガーディアンのレンダリング', async () => {
   const handleToggleAt = vi.fn()
-  const { findByRole } = render(
+  const { rerender, findByRole } = render(
     <ImageCardWithToggle
       toggle={enumToggle.OPAQUE}
       handleToggleAt={handleToggleAt}
       index={0}
     />
   )
-  const buttonToggle = await findByRole('button')
+  expect(handleToggleAt.mock.calls.length).toBe(0)
+
+  let buttonToggle = await findByRole('button')
   expect(buttonToggle).toBeVisible()
   expect(buttonToggle).toHaveClass('btn-toggled-opaque')
 
@@ -24,4 +26,236 @@ test('レンダリング', async () => {
   expect(handleToggleAt.mock.calls.length).toBe(1)
   expect(handleToggleAt.mock.lastCall.length).toBe(1)
   expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.TRANSPARENT}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-transparent')
+
+  await userEvent.click(buttonToggle)
+  expect(handleToggleAt.mock.calls.length).toBe(2)
+  expect(handleToggleAt.mock.lastCall.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.RED}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-red')
+
+  await userEvent.click(buttonToggle)
+  expect(handleToggleAt.mock.calls.length).toBe(3)
+  expect(handleToggleAt.mock.lastCall.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.BLUE}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-blue')
+
+  await userEvent.click(buttonToggle)
+  expect(handleToggleAt.mock.calls.length).toBe(4)
+  expect(handleToggleAt.mock.lastCall.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.YELLOW}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-yellow')
+
+  await userEvent.click(buttonToggle)
+  expect(handleToggleAt.mock.calls.length).toBe(5)
+  expect(handleToggleAt.mock.lastCall.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.WHITE}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-white')
+
+  await userEvent.click(buttonToggle)
+  expect(handleToggleAt.mock.calls.length).toBe(6)
+  expect(handleToggleAt.mock.lastCall.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.BLACK}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-black')
+
+  await userEvent.click(buttonToggle)
+  expect(handleToggleAt.mock.calls.length).toBe(7)
+  expect(handleToggleAt.mock.lastCall.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.TRANSPARENT}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-transparent')
+})
+
+test('初期手札のレンダリング', async () => {
+  const handleToggleAt = vi.fn()
+  const { rerender, findByRole } = render(
+    <ImageCardWithToggle
+      toggle={enumToggle.TRANSPARENT}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+  expect(handleToggleAt.mock.calls.length).toBe(0)
+
+  let buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-transparent')
+
+  await userEvent.click(buttonToggle)
+  expect(handleToggleAt.mock.calls.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.RED}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-red')
+
+  await userEvent.click(buttonToggle)
+  expect(handleToggleAt.mock.calls.length).toBe(2)
+  expect(handleToggleAt.mock.lastCall.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.BLUE}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-blue')
+
+  await userEvent.click(buttonToggle)
+  expect(handleToggleAt.mock.calls.length).toBe(3)
+  expect(handleToggleAt.mock.lastCall.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.YELLOW}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-yellow')
+
+  await userEvent.click(buttonToggle)
+  expect(handleToggleAt.mock.calls.length).toBe(4)
+  expect(handleToggleAt.mock.lastCall.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.WHITE}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-white')
+
+  await userEvent.click(buttonToggle)
+  expect(handleToggleAt.mock.calls.length).toBe(5)
+  expect(handleToggleAt.mock.lastCall.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.BLACK}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-black')
+
+  await userEvent.click(buttonToggle)
+  expect(handleToggleAt.mock.calls.length).toBe(6)
+  expect(handleToggleAt.mock.lastCall.length).toBe(1)
+  expect(handleToggleAt.mock.lastCall[0]).toBe(0)
+
+  rerender(
+    <ImageCardWithToggle
+      toggle={enumToggle.TRANSPARENT}
+      handleToggleAt={handleToggleAt}
+      index={0}
+    />
+  )
+
+  buttonToggle = await findByRole('button')
+  expect(buttonToggle).toBeVisible()
+  expect(buttonToggle).toHaveClass('btn-toggled-transparent')
 })
