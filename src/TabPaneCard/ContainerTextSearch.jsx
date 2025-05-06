7413fdcade45fbe36e9514a11d68c74146ac834e
sweetpotato <sweetpotato@users.noreply.github.com>
2025-05-06 19:00:10 +0900

    js, test: 検索ボックスにクリアボタンを追加
    Close #59

--- src/TabPaneCard/ContainerTextSearch.jsx
+++ src/TabPaneCard/ContainerTextSearch.jsx
@@ -1,18 +1,49 @@
 // SPDX-License-Identifier: MIT
 
-import { FormCheck, FormControl } from 'react-bootstrap'
+import { useRef } from 'react'
+import { Button, FormCheck, FormControl, InputGroup } from 'react-bootstrap'
 
 function ContainerTextSearch({
   handleChangeKeywords,
   handleChangeIncludesTraitAndLegacy,
 }) {
+  const ref = useRef(null)
+
+  function handleClickClear() {
+    /*
+     * テキストボックスの現在のテキストを state として保持し、
+     * FormControl に value={state} を指定して
+     * 入力値が変わるたびに再レンダリングするように
+     * このコンポーネントを実装してみたところ、
+     * 単体テストにおける userEvent.type() 実行後の
+     * アサーションを自然に書くことができなかった。
+     *
+     * そこで value={state} は指定せず、代わりに、
+     * クリアボタンが押されたときに
+     * テキストボックスを空にするイベントを模倣し、
+     * handleChangeKeywords() にそれらしい引数を与えて呼び出す。
+     * これは当該関数の実装が既知であることを前提としたコードであり、
+     * 決してキレイではないどころかむしろ汚いが、
+     * 意識すべき範囲は useContainerTextSearch までと
+     * 比較的小さく閉じていることから、この実装を採用する。
+     */
+    ref.current.value = ''
+    handleChangeKeywords({ currentTarget: { value: '' } })
+  }
+
   return (
     <>
       <div className="m-2">
-        <FormControl
-          placeholder="カード名やルールテキストを入力して検索"
-          onChange={handleChangeKeywords}
-        />
+        <InputGroup>
+          <FormControl
+            ref={ref}
+            placeholder="カード名やルールテキストで検索"
+            onChange={handleChangeKeywords}
+          />
+          <Button variant="outline-danger" onClick={handleClickClear}>
+            クリア
+          </Button>
+        </InputGroup>
       </div>
       <div className="m-2">
         <FormCheck
